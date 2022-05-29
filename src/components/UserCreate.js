32b5f99a2f061349ed0689f14b4a5f975741e5a8
Gee <glynester@hotmail.com>
2022-05-29 21:58:41 +0100

    Components created and form markup done

---
+++ src/components/UserCreate.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Field from './Field';
+import Button from './Button';
+
+const UserCreate=()=>{
+  return (
+    <div className='ui form'>
+      {/* UserCreate File */}
+      <Field />
+      <Button />
+    </div>
+  )
+}
+
+export default UserCreate;
+
+
+
+
+
