d1b16b1ccd7f7ff04cef53118bb88e78a2f560f8
Gee <glynester@hotmail.com>
2022-05-31 21:06:29 +0100

    Refactored from Context Provider to now access data with a Consumer

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,13 +5,24 @@ class Button extends React.Component{
   // Must be called 'contextType'.
   // static adds a property directly to the class itself;
   // Code below is the same as writing 'Button.contextType= LanguageContext;'
-  static contextType = LanguageContext;
+  // static contextType = LanguageContext;  // Context type is only required when we want to get our data assigned to the this.context property.
+
+  // Helper method
+  renderSubmit(value){
+    return value === 'english'?'Submit':'Voorleggen';
+  }
   
   render(){
-    console.log("this.context=>",this.context);
-    const text = this.context === 'english'?'Submit':'Voorleggen';
+    // console.log("this.context=>",this.context);
+    // const text = this.context === 'english'?'Submit':'Voorleggen';
     return (
-      <button className='ui button primary'>{text}</button>
+      <button className='ui button primary'>
+        {/* {text} */}
+        {/* We are providing a function as a child to a real component. That component is going to take that child function and automatically invoke it for us. */}
+        <LanguageContext.Consumer>
+          {(value)=>this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
     )
   }
 }
